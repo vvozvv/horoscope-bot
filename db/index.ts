0dbbed194a193ea9dfc4f7c3527ef0f47aacf710
vvozvv <a.vvozvv@gmail.com>
2024-02-01 16:41:32 +0300

    Добавлена кнопка "Заявки в бота" и функционал для нее

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/psb-booking-db')
+  // .connect('mongodb://127.0.0.1:27017/psb-booking-db')
+  .connect('mongodb://gen_user:I(0l_d_%2Fhdr%24%255@147.45.107.117:27017/default_db?authSource=admin&directConnection=true')
   .then(() => console.info('Mongodb connected!'))
   .catch(e => {
     console.error('Mongodb connection error', e.message);
